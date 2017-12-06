5c13b6914ed0ea1fa3d3a9e66e0e16dfabb105c2
Carlos Treminio <ctreminiom079@gmail.com>
2017-12-05 21:21:28 -0600

    add token validation.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,30 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Auth from '@/packages/auth/auth.js'
+
+Vue.use(Auth)
+
 Vue.config.productionTip = false
+
+router.beforeEach(
+  (to, from, next) => {
+
+    if(to.matched.some( record => record.meta.forVisitors)) 
+    {
+      if (Vue.auth.isAuthenticated())
+      {
+        next({
+
+          path: '/dashboard'
+        })
+      }
+      else next()
+    } else next()
+
+  }
+)
+
 
 /* eslint-disable no-new */
 new Vue({
